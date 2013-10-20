26fb8094eb088117c61f7166d16ff44ff6789744
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2013-10-20 21:33:16 +0200

    added validation

--- src/clj/graph.clj
+++ src/clj/graph.clj
@@ -2,4 +2,15 @@
 
 (defrecord Graph [vertices edges])
 
-(defn graph [vertices edges] (->Graph vertices edges))
+(defn- valid-edges?
+  ([edges]
+     (if (empty? edges)
+       true
+       (let [valids (map #(and (set? %) (= 2 (count %))) edges)]
+         (reduce #(and %1 %2) valids)))))
+
+(defn graph [vertices edges]
+  (if (and (set? vertices) (valid-edges? edges))
+    (->Graph vertices edges)
+    (throw (Exception. "Invalid graph"))))
+
--- test/clj/graph_test.clj
+++ test/clj/graph_test.clj
@@ -7,5 +7,16 @@
     (is (not (nil? (graph #{} #{})))))
   (testing "can-associate-sets"
     (is (= #{:v} (:vertices (graph #{:v} #{}))))
-    (is (= #{#{:a, :b}} (:edges (graph #{} #{#{:a, :b}}))))))
+    (is (= #{#{:a, :b}} (:edges (graph #{} #{#{:a :b}}))))))
 
+(deftest can-validate-graph
+  (testing "can-recognise-invalid-vertices"
+    (try (do
+           (graph {} #{#{:a :b}})
+           (is false "Vertices validation not working"))
+         (catch Exception e true))) 
+  (testing "can-recognise-invalid-edges"
+    (try (do
+           (graph #{} #{#{:a}})
+           (is false "Edges validation not working"))
+         (catch Exception e true))))
