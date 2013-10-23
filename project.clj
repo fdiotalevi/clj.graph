932d46c9592627c98563831938195b4294350183
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2013-10-23 09:02:43 +0100

    index page

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject clj.graph "0.1.0-SNAPSHOT"
-  :description "Graph manipulation library in Clojure"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-marginalia "0.7.1"]]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [lein-marginalia "0.7.1"]])
